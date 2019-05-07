e37e57352b6273948ad0274fcdecedac4494d7fe
Jonathan Campos <jonathan.campos.net@gmail.com>
2019-05-07 20:47:45 -0300

    Projeto criado

---
+++ src/main/java/com/jonathancampos/springproject/JavaspringprojectApplication.java
@@ -0,0 +1,13 @@
+package com.jonathancampos.springproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaspringprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaspringprojectApplication.class, args);
+	}
+
+}
